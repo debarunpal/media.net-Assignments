59a2296c122cd2920bf10d5491eb2aec3d2314d8
Debarun Pal <debarunpal@gmail.com>
2020-08-16 16:47:10 +0530

    Making Dynamic API Fetch Request Response Behaviour

--- Assignment 1 - Optimized Pixel Handler/js/db.js
+++ Assignment 1 - Optimized Pixel Handler/js/db.js
@@ -1,12 +1,23 @@
+// Offline Data Behaviour
+db.enablePersistence()
+    .catch(err => {
+        if (err.code == 'failed-precondition') {
+            console.log('Persistence Failed!'); // Maybe multiple tabs are open at once
+        } else if (err.code == 'unimplemented') {
+            console.log('Persistence is not available'); // Browser does not support the feature
+        }
+    });
+
 // Adding a Real Time Listener
 db.collection('recipes').onSnapshot((snapshot) => {
     // console.log(snapshot.docChanges());
     snapshot.docChanges().forEach((change) => {
         // console.log(change, change.doc.data(), change.doc.id);
-        if(change.type === 'added') {
-            //To-do
+        if (change.type === 'added') {
+            // Add the Document Data to the Web Page
+            renderRecipe(change.doc.data(), change.doc.id);
         }
-        if(change.type === 'removed') {
+        if (change.type === 'removed') {
             //To-do
         }
     })
